5841bc7ea73e4544c42a097b1d0b5911cc5c69f0
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-07-12 01:35:35 +0100

    ✨ first working LSP, with completion.

---
+++ squirrel-prover-lsp/init.lua
@@ -0,0 +1,39 @@
+local lsp = require 'easy-lsp'
+
+local tactics = require 'squirrel-prover-lsp.tactics'
+
+lsp.name = "Squirrel-Prover LSP"
+lsp.version = "0.1.0"
+lsp.capabilities = {
+	textDocumentSync = 1,
+	completionProvider = {},
+	hoverProvider = true,
+}
+
+lsp.HandleRequest["textDocument/hover"] = function (request)
+	local response = {
+		id = request.id,
+		result = {
+			contents = {
+				kind = 'markdown',
+				value = "# Tactic __auto__ _simpl_flags_\nAttempt to automatically prove a goal using the hypotheses.\nThe tactic uses the __reduction engine__ with the provided flags (defaults to :g:`rw,beta,proj`).\nMoreover, for local goals, the tactic relies on basic propositional reasoning, rewriting simplications, and both __constraints__ and __congruence__.\n---\n___Error:___ cannot close goal\nThe current goal could not be closed.",
+			},
+		}
+	}
+	return response
+end
+
+function lsp.generate_completion (params)
+	local results = {}
+	for _, tactic in pairs(tactics) do
+		local item = {
+			label = tactic.name,
+			documentation = tactic.description,
+			kind = 2
+		}
+		table.insert(results, item)
+	end
+	return results
+end
+
+lsp.start()
